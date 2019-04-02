f2d0890f94baa8990a4a2abff1f2f216e9c523e8
DESKTOP-NNQEDID\ttehb <ttehbb@gmail.com>
2019-04-03 00:01:14 +0300

    Adds ping controller

---
+++ api/receipts-server/ReceiptsServer/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReceiptsServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello");
+        }
+    }
+}
