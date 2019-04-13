723d60f5e0dae60ce490998efce946d75c1d4ab3
DESKTOP-NNQEDID\ttehb <ttehbb@gmail.com>
2019-04-13 15:56:33 +0300

    Clean up

--- api/receipts-server/ReceiptsServer/ApplicationContext.cs
+++ api/receipts-server/ReceiptsServer/ApplicationContext.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ReceiptsServer.EF.Model;
 
 namespace ReceiptsServer
--- api/receipts-server/ReceiptsServer/Controllers/ReadReceiptController.cs
+++ api/receipts-server/ReceiptsServer/Controllers/ReadReceiptController.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CheckReceiptSDK;
-using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json.Linq;
 using ReceiptsServer.Model;
 
 namespace ReceiptsServer.Controllers
